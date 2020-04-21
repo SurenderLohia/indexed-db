84d4b6fe50f2454bb14e51e9c58663ec53560bd5
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-21 17:03:58 +0530

    Indexed db basic setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import './index.css';
-console.log('hello webpack');
+import { openDB } from 'idb/with-async-ittr.js';
 
-if (!window.indexedDB) {
-  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+async function demo() {
+  const db = await openDB('Articles', 2, {
+    upgrade(db) {
+      const store = db.createObjectStore('articles', {
+        keyPath: 'id',
+        autoIncrement: true
+      });
+
+      store.createIndex('date', 'date');
+
+      // Add multiple articles in one transaction:
+      store.transaction.oncomplete = async function(event) {
+        const tx = db.transaction('articles', 'readwrite');
+        tx.store.add({
+          title: 'Article 2',
+          date: new Date('2019-01-01'),
+          body: '…',
+        });
+        tx.store.add({
+          title: 'Article 3',
+          date: new Date('2019-01-02'),
+          body: '…',
+        });
+        await tx.done;
+      }
+    }
+  });
+
+  console.log(await db.getAll('articles'));
 }
+
+demo();
