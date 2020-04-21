a78fa9a6d60b8898c2b376f8d6ddf90eb96717a1
Surender Lohia <surender.lohia.e@gmail.com>
2020-04-21 10:03:25 +0530

    Webpack setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './index.css';
+console.log('hello webpack');
+
+if (!window.indexedDB) {
+  console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+}
